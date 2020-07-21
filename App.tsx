4915f2b3248612783c330a82eb2070473dfdcb4c
Raghuveer Bharadwaj <raghuveer.mh@cognitiveclouds.com>
2020-07-21 08:51:26 +0530

    One Line Commit

--- App.tsx
+++ App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux'
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import store from './redux/store'
 import Nav from './navigation/Navigation'
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'tomato',
-    accent: 'yellow',
-    dark: true
-  },
-};
 
 export default function App() {
   return (
     <Provider store={store}>
-      <PaperProvider theme={theme}>
         <Nav />
-      </PaperProvider>
     </Provider>
   );
 }
